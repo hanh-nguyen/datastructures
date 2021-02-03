b2df80e4b2cee1fa5d3acd27693a67e3307357ed
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2021-02-02 19:41:16 -0500

    submit func find_files

---
+++ filerecursion.py
@@ -0,0 +1,28 @@
+import os
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    if os.path.isfile(path):
+        if path.endswith(suffix):
+            return [path]
+    elif os.path.isdir(path):
+        results = []
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+            result = find_files(suffix, item_path)
+            if result:
+                results += result
+        return results
